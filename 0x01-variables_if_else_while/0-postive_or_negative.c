c59040d6d51f48b2efe6083512356f6443a54a5a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-08-27 21:39:31 +0100

    added a readme file and a program to print whether a random int is positive, zero or negative

---
+++ 0x01-variables_if_else_while/0-postive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random number each time and prints it
+ * none
+ * Return: success value 0  
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is postive\n", n)
+	else if (n = 0)
+		printf("%d is zero\n", n)
+	else
+		printf("%d is negative\n", n)
+	return (0);
+}
