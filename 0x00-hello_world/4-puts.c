939f71995b5d7f75fb0e6d00eb4c1010c088db14
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-08-27 11:06:02 +0100

    c program to print a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
+#main function that contains a program to print a given string
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
