dc9fa14c9314844f671303572600f6682c109e74
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-08-28 06:07:22 +0100

    c program to check if the last digit of a random int is equal to certain pre-defined values

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,14 +10,18 @@
 int main(void)
 {
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
-		printf("Last digit of n is %s \n", 'greater than 5');
-	else if ((n % 10) == 0)
-		printf("Last digit of n is %s \n", 'and is 0');
-	else if (((n % 10) > 0) && ((n % 10) < 6))
-		printf("Last digit of n is %s \n", 'greater than 5);
+
+	last = n % 10;
+
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if ((last != 0) && (last < 6))
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
