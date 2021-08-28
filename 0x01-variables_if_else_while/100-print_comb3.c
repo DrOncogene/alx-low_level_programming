150a0ba081e1c4ce38995b0b2b80bb2266ae19db
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-08-28 18:00:01 +0100

    c program to print all possible combinations of 2 digits except when they are equal

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two digits numbers without repetitions
+ * main - prints all possible combinations of two digits numbers without
+ repetitions
  * none
  * Return: success number 0
  */
