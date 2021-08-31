534c4f36c60fc05bc4f1ec4f84acecf22163c6df
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-08-31 19:32:41 +0100

    function to check whether a given char is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a given int parameter is a letter
+ * @c: int parameter to be checked
+ * Return: 1 if c is a letter, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	int i;
+
+	i = 65;
+	while (i <= 90)
+	{
+		int j;
+
+		for (j = 97; j <= 122; j++)
+		{
+			if ((c == j) || (c == i))
+				return (1);
+		}
+		j++
+	}
+	return (0)
+}
