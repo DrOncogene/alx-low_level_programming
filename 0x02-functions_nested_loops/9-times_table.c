68d0b4e6cede7cb6e31ca46e2d8de1f59bc89779
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-01 07:12:32 +0100

    program to print the 9 times table starting from 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ * none
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i;
+	
+	i = 0;
+	while (i < 10)
+	{
+		int j;
+
+		j = 0;
+		while (j < 10)
+		{
+			char prod;
+
+			prod = i * j;
+			if ((prod / 10) != 0)
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if ((prod /10) == 0)
+				_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
