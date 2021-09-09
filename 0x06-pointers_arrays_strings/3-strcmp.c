8bb87dfeabad60834ea73c18aaa2341ff0863418
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-09 13:12:57 +0100

    function to compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,13 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: negative, 0 or positive int if s1 is less than, matches or greater than s2
+ * Return: neg, 0, or pos int if s1 is less than, matches or greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
 	int res;
 
-	do
-	{
+	do {
 		if (*s1 == *s2)
 		{
 			res = 0;
