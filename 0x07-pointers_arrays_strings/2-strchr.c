956192b2a0a0e3a1ac66be8d6d26a02cd7d90675
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-14 12:02:58 +0100

    fixed 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,28 @@
  */
 char *_strchr(char *s, char c)
 {
+	int len_s, i;
+
+	len_s = 0;
 	while (*s != 0)
 	{
+		len_s++;
+		s++;
+	}
+
+	s -= len_s;
+	for (i = 0; i <= len_s; i++)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	/*{
 		if (*s == c)
 			return (s);
 
 		s++;
-	}
+	}*/
 
 	return ('\0');
 }
