826d32c8e4ce1b5d1d19eb6fc5836438773ca0f7
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-18 00:25:32 +0100

    adds any number of positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
+			if ((*argv[i] < 48 || *argv[i] > 57) && *argv[i] != '-')
 			{
 				printf("%s\n", "Error");
 				return (1);
