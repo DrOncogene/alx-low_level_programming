27294c124398be06249b0814a788860f096bb0bd
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-19 04:57:35 +0100

    returns the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
+#include <stdio.h>
 
-int len = 0;
 /**
  * _strlen_recursion - finds the length of a string
  * @s: the string
@@ -8,10 +8,8 @@ int len = 0;
  */
 int _strlen_recursion(char *s)
 {
-	if (*s)
-	{
-		len++;
-		_strlen_recursion((char *)(s + 1));
-	}
-	return (len);
+	if (*s == 0)
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
 }
