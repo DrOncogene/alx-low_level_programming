bebca054ba3ecfc04da4905a1bcdcc91f9a0fda9
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-23 19:26:49 +0100

    concat n chars from a string to another

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+unsigned int len(char *s);
+/**
+ * string_nconcat - concatenates n chars from s2 to s1
+ * @s1: first string
+ * @s2: second string
+ * @n: n of chars to be concatenated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s, *s_hold;
+	unsigned int len_s1, len_s, i, j;
+
+	len_s1 = len(s1);
+	if (n >= len(s2))
+		len_s = len(s1) + len(s2);
+	else
+		len_s = len_s1 + n;
+	s = malloc(sizeof(char) * (len_s + 1));
+	s_hold = s;
+
+	if (s != NULL)
+	{
+		for (i = 0; i < len_s1; i++)
+		{
+			*(s_hold + i) = *(s1 + i);
+		}
+
+		for (j = 0; j < n; j++, i++)
+			*(s_hold + i) = *(s2 + j);
+		*(s_hold + i) = '\0';
+	}
+
+	return (s);
+}
+
+/**
+ * len - finds the length of a string
+ * @s: the string
+ * Return: len of s
+ */
+unsigned int len(char *s)
+{
+	if (*s == 0 || s == NULL)
+		return (0);
+	return (len(s + 1) + 1);
+}
