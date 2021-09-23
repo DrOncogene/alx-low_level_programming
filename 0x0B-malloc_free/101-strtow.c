d6dc03cbddaeee99d5436ccb8bb6be1f78b49ba6
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-23 10:41:02 +0100

    rewrote part of the function to fix overallocation of memory and created a new function

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,6 +4,7 @@
 
 int word_count(char *s);
 int word_len(char *w);
+void set_string(char **, char *, int, int);
 /**
  * strtow - creates an array of words for a string delimited by spaces
  * @str: the string
@@ -12,7 +13,7 @@ int word_len(char *w);
 char **strtow(char *str)
 {
 	char **s, *str_h;
-	int words, i, j, len_word;
+	int words, i, len_word;
 
 	words = word_count(str);
 	str_h = str;
@@ -27,25 +28,16 @@ char **strtow(char *str)
 			len_word = 0;
 			if (str == str_h && *str != ' ' && *str != 0)
 			{
+
 				len_word = word_len(str);
-				s[i] = malloc(sizeof(char) * len_word);
-				if (s[i] != NULL)
-				{
-					for (j = 0; j < len_word; j++)
-						s[i][j] = *(str + j);
-					i++;
-				}
+				set_string(s, str, i, len_word);
+				i++;
 			}
 			else if (*str == ' ' && *(str + 1) != ' ' && *(str + 1) != 0)
 			{
 				len_word = word_len(str + 1);
-				s[i] = malloc(sizeof(char) * len_word);
-				if (s[i] != NULL)
-				{
-					for (j = 0; j < len_word; j++)
-						s[i][j] = *(str + 1 + j);
-					i++;
-				}
+				set_string(s, str + 1, i, len_word);
+				i++;
 			}
 			if (len_word == 0)
 				str++;
@@ -79,7 +71,7 @@ int word_count(char *s)
 		else if (*s == ' ' && *(s + 1) != ' ' && *(s + 1) != 0)
 		{
 			count++;
-			s += word_len(s + 1);
+			s += word_len(s + 1) + 1;
 		}
 		else
 			s++;
@@ -98,4 +90,24 @@ int word_len(char *w)
 	if (*w == ' ' || *w == 0)
 		return (0);
 	return (word_len(w + 1) + 1);
+}
+
+/**
+ * set_string - copy a string of len chars from src to dest array at position i
+ * @dest: destination array
+ * @src: the string
+ * @i: position of string in dest
+ * @len: length of string
+ * Return: nothing
+ */
+void set_string(char **dest, char *src, int i, int len)
+{
+	int j;
+
+	dest[i] = malloc(sizeof(char) * len);
+	if (dest[i] != NULL)
+	{
+		for (j = 0; j < len; j++)
+			dest[i][j] = *(src + j);
+	}
 }
