a9a6d85182bc4b891e99d7227b10d480ca683f8a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-24 21:59:06 +0100

    minor tweaks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,17 +8,32 @@ unsigned int len(char *s);
  * @s1: first string
  * @s2: second string
  * @n: n of chars to be concatenated
+ * Return: pointer to the result
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s, *s_hold;
-	unsigned int len_s1, len_s, i, j;
+	unsigned int len_s1, len_s2, len_s, i, j;
 
-	len_s1 = len(s1);
-	if (n >= len(s2))
+	if (s1 == NULL && s2 != NULL)
+		len_s1 = 0;
+	else if (s2 == NULL && s1 != NULL)
+		len_s2 = 0;
+	else if (s1 == NULL && s2 == NULL)
 	{
-		n = len(s2);
-		len_s = len(s1) + len(s2);
+		len_s1 = 0;
+		len_s2 = 0;
+	}
+	else
+	{
+		len_s1 = len(s1);
+		len_s2 = len(s2);
+	}
+
+	if (n >= len_s2)
+	{
+		n = len_s2;
+		len_s = len_s1 + len_s2;
 	}
 	else
 		len_s = len_s1 + n;
@@ -27,13 +42,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s != NULL)
 	{
-		for (i = 0; i < len_s1; i++)
-		{
-			*(s_hold + i) = *(s1 + i);
-		}
-
-		for (j = 0; j < n; j++, i++)
-			*(s_hold + i) = *(s2 + j);
+		i = 0;
+		if (len_s1 > 0)
+			for (i = 0; i < len_s1; i++)
+				*(s_hold + i) = *(s1 + i);
+		if (n > 0)
+			for (j = 0; j < n; j++, i++)
+				*(s_hold + i) = *(s2 + j);
 		*(s_hold + i) = '\0';
 	}
 	else
@@ -49,7 +64,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  */
 unsigned int len(char *s)
 {
-	if (*s == 0 || s == NULL)
+	if (*s == 0)
 		return (0);
 	return (len(s + 1) + 1);
 }
