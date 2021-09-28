e62b65a0893a871fb80797b33399a3082789bb2a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-28 10:47:56 +0100

    minor fixes

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdddef.h>
 
 /**
  * free_dog - frees the memory allocated to a previously created dog
@@ -8,6 +9,8 @@
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);
