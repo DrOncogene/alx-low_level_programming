cd8ca5d9ee00c9979ad801b858d29b9d6bc53a35
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-09-30 20:57:39 +0100

    prints any number of given integers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,13 @@
+#include "variadic function.h"
+#include <stdarg.h>
+
+/**
+  * print_numbers - print a given set of numbers with a separator
+  * @separator: the separator
+  * @n: number of optional args passed
+  * Return: nothing
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
