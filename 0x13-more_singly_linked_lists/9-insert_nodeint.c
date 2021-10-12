c0f2a140755bbdcd37a8a2d574101c90e1d15d59
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-12 21:39:33 +0100

    minor fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *prev, *next, *head_hold;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (*head == NULL && idx > 0)
 		return (NULL);
 
 	head_hold = *head;
