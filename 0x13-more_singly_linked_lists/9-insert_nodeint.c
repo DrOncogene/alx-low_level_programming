cf425ac69ec52c38591a948217ba69770f1f3f75
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-12 22:18:49 +0100

    fixed index out of range

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,12 +12,13 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *prev, *next, *head_hold;
-	unsigned int i;
+	unsigned int i, len;
 
-	if (*head == NULL && idx > 0)
+	len = len_list(*head);
+	head_hold = *head;
+	if (*head == NULL || index > len - 1)
 		return (NULL);
 
-	head_hold = *head;
 	new = malloc(sizeof(listint_t));
 	if (new != NULL)
 	{
@@ -48,4 +49,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	return (new);
+}
+
+/**
+  * len_list - finds the length of a list
+  * @head: head of the list
+  * Return: the length
+  */
+unsigned int len_list(listint_t *head)
+{
+	listint_t *head_hold;
+	unsigned int len;
+
+	len = 0;
+	head_hold = head;
+	while (head)
+	{
+		len++;
+		head = head->next;
+	}
+
+	head = head_hold;
+	return (len);
 }
