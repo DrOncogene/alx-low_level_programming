ba739bd9258183b8dd1ac005f0fd0036d72b86a2
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-15 13:12:37 +0100

    clears a bit at an index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * set_bit - finds and returns a bit at an index
+  * clear_bit - finds and returns a bit at an index
   * @n: the number
   * @index: index of bit to be returned
   * Return: bit at index or -1
@@ -10,13 +10,13 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (*n == 0 && index == 0)
 	{
-		*n |= 1 << index;
+		*n &= 0 << index;
 		return (1);
 	}
 	else if (*n == 0 || index > 63)
 		return (-1);
 
-	*n |= 1 << index;
+	*n &= 0 << index;
 
 	return (1);
 }
