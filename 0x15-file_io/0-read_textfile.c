b6ef058b6db0d2353197a4e6ffbd12c757c47ba6
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-18 21:30:16 +0100

    function the reads and prints a number of chars from a text file to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+  * read_textfile - reads a text file and prints it to the stdout
+  * @filename: name of the file
+  * @letters: number of letters to be read and printed
+  * Return: number of letters able to be read and printed
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n_char, fd;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	fd = open(filename, O_RDONLY, 755);
+	if (fd == -1)
+		return (0);
+
+	fd = read(fd, buf, letters);
+	if (fd == -1)
+		return (0);
+
+	n_char = write(1, buf, letters);
+	if (n_char == -1 || (n_char < fd))
+		return (0);
+
+	fd = close(fd);
+
+	return (n_char);
+}
