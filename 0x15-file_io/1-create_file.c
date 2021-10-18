5121ef6f5086c0d2ac924b34a2916acbed5aae73
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-18 22:19:43 +0100

    function that creates a file with a given text

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int len(char *s);
+/**
+  * create_file - creates a file with a given text
+  * @filename: name of the file
+  * @text_content: content of the file
+  * Return: 1 on success, -1 otherwise
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, text_content, len(text_content));
+	if (w == -1)
+		return (-1);
+
+	return (1);
+}
+
+/**
+  * len - computes the length of a string
+  * @s: the string
+  * Return: len of s
+  */
+int len(char *s)
+{
+	if (s == NULL || *s == 0)
+		return (0);
+
+	return (len(s + 1) + 1);
+}
