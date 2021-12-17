5b43c72384075749d07a7c9153ee21142ad09bfd
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-17 21:18:47 +0100

    added buffering to the function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,33 +13,37 @@ void hash_table_print(const hash_table_t *ht)
 
 	if (ht == NULL)
 		return;
-
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 		return;
-	write(1, "{", 1);
+	buffer[0] = '\0';
+	strcat(buffer, "{");
 	for (i = 0; i < ht->size; i++)
 	{
 		current = (ht->array)[i];
 		if (current == NULL)
 			continue;
 		else
-		{
 			while (current)
 			{
-				if (strlen(buffer) > 1000)
-					buffer = realloc(buffer, 1024);
-				if (buffer == NULL)
-					return;
 				strcat(buffer, "'");
 				strcat(buffer, current->key);
 				strcat(buffer, "': '");
 				strcat(buffer, current->value);
 				strcat(buffer, "', ");
+				if (strlen(buffer) > 1000)
+				{
+					buffer = realloc(buffer, sizeof(buffer) + 1024);
+					if (buffer == NULL)
+					{
+						free(buffer);
+						return;
+					}
+				}
 				current = current->next;
 			}
-		}
 	}
 	write(1, buffer, strlen(buffer) - 2);
+	free(buffer);
 	write(1, "}\n", 2);
 }
