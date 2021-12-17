83671b1bd423c2cb5f091b45bfbc40268f608881
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-17 17:31:36 +0100

    function that adds a key-value pair to or update a key in a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,38 +9,44 @@
   */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new, *current;
+	unsigned long int index, key_exist;
+	hash_node_t *new, *current, *prev;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	current = (ht->array)[index];
-	while (current)
-	{
-		if (current->key == (char *)key)
-		{
-			free(current->value);
-			current->value = strdup(value);
-			return (1);
-		}
-		current = current->next;
-	}
-
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-
 	new->key = (char *)key;
 	new->value = strdup(value);
 	if ((ht->array)[index] == NULL)
 		new->next = NULL;
 	else
+	{
+		key_exist = 0;
+		prev = NULL;
+		current = (ht->array)[index];
+		while (current)
+		{
+			if (current->key == (char *)key)
+			{
+				key_exist = 1;
+				break;
+			}
+			prev = current;
+			current = prev->next;
+		}
+		if (prev && key_exist)
+		{
+			prev->next = current->next;
+			free(current);
+		}
+		else if (prev == NULL && key_exist)
+			free(current);
 		new->next = (ht->array)[index];
-
+	}
 	(ht->array)[index] = new;
-
 	return (1);
 }
