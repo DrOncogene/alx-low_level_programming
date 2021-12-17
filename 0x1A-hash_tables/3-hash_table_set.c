2f38d290289e2a2b5a3375d5bd5a83737853808a
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-17 13:02:25 +0100

    function that adds a key-value pair to an hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - adds an element to a hash table
+  * @ht: the hash table
+  * @key: the new key
+  * @value: value to be stored
+  * Return: 1 if success, 0 otherwise
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+
+	if (ht == NULL || strlen(key) == 0 || key == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = (char *)key;
+	new->value = strdup(value);
+	if ((ht->array)[index] == NULL)
+		new->next = NULL;
+	else
+		new->next = (ht->array)[index];
+
+	(ht->array)[index] = new;
+
+	return (1);
+}
