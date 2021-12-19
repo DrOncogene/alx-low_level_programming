67e21d4786fbbe17c514927207625165d1352f19
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-12-19 20:14:10 +0100

    minor correction

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -200,6 +200,9 @@ void shash_table_print_rev(const shash_table_t *ht)
 	shash_node_t *current;
 	char *buffer;
 
+	if (ht == NULL)
+		return;
+
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 		return;
